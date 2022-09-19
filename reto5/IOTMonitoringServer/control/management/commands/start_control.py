b801470dfe3ad25eb17597dd6c59aa632cb759f1
ccolorado1392 <c.colorado@uniandes.edu.co>
2022-09-19 18:18:32 -0500

    Update start_control.py

--- reto5/IOTMonitoringServer/control/management/commands/start_control.py
+++ reto5/IOTMonitoringServer/control/management/commands/start_control.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
 from control import monitor
-
+from threading import Thread
 
 class Command(BaseCommand):
     help = 'Starts actuators control'
 
     def handle(self, *args, **kwargs):
         monitor.setup_mqtt()
-        monitor.start_cron_warning()
-        monitor.start_cron_alert()
+        Thread(target = monitor.start_cron_warning()).start()
+        Thread(target = monitor.start_cron_alert()).start()
+        
+        
        
