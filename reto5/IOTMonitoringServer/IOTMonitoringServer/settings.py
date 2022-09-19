f6c2d1196745c59c9fec4e8fd68c4fcad30a9c8a
ccolorado1392 <c.colorado@uniandes.edu.co>
2022-09-19 14:20:36 -0500

    Update settings.py

--- reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.232.129.83"]
+ALLOWED_HOSTS = ["localhost", "35.174.242.92"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "35.172.233.55",  # Dirección IP de la base de datos
+        "HOST": "44.195.66.0",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.239.146.152"
+MQTT_HOST = "3.92.19.66"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
