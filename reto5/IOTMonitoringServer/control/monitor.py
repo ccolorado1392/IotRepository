c6010b0eecc08606f5de73d0ebc227d4fb89b9aa
ccolorado1392 <c.colorado@uniandes.edu.co>
2022-09-19 18:28:42 -0500

    Update monitor.py

--- reto5/IOTMonitoringServer/control/monitor.py
+++ reto5/IOTMonitoringServer/control/monitor.py
@@ -18,36 +18,37 @@ def analyze_warning():
 
     print("Calculando Warning...")
 
-    # data = Data.objects.filter(
-    #     base_time__gte=datetime.now() - timedelta(minutes=10))
-    # aggregation = data.annotate(check_value=Avg('avg_value')) \
-    #     .select_related('station', 'measurement') \
-    #     .select_related('station__user', 'station__location') \
-    #     .select_related('station__location__city', 'station__location__state',
-    #                     'station__location__country') \
-    #     .values('check_value', 'station__user__username',
-    #             'measurement__name',
-    #             'measurement__max_value',
-    #             'measurement__min_value',
-    #             'station__location__city__name',
-    #             'station__location__state__name',
-    #             'station__location__country__name')
-    # alerts = 0
+    data = Data.objects.filter(
+        base_time__gte=datetime.now() - timedelta(minutes=10))
+    aggregation = data.annotate(check_value=Avg('avg_value')) \
+        .select_related('station', 'measurement') \
+        .select_related('station__user', 'station__location') \
+        .select_related('station__location__city', 'station__location__state',
+                        'station__location__country') \
+        .values('check_value', 'station__user__username',
+                'measurement__name',
+                'measurement__max_value',
+                'measurement__min_value',
+                'station__location__city__name',
+                'station__location__state__name',
+                'station__location__country__name')
+    alerts = 0
 
-    # for item in aggregation:
-    #     alert = False
+    for item in aggregation:
+        alert = False
 
-    #     variable = item["measurement__name"]
-    #     max_value = item["measurement__max_value"] or 0
-    #     min_value = item["measurement__min_value"] or 0
+        variable = item["measurement__name"]
+        max_value = item["measurement__max_value"] or 0
+        min_value = item["measurement__min_value"] or 0
 
-    #     country = item['station__location__country__name']
-    #     state = item['station__location__state__name']
-    #     city = item['station__location__city__name']
-    #     user = item['station__user__username']
+        country = item['station__location__country__name']
+        state = item['station__location__state__name']
+        city = item['station__location__city__name']
+        user = item['station__user__username']
 
-    #     # if item["check_value"] > (max_value - 5) or item["check_value"] < (min_value + 5):
-    #     #         alert = True
+        # if item["check_value"] > (max_value - 5) or item["check_value"] < (min_value + 5):
+        #         alert = True
+        
     alert = True
 
     if alert:
