d13a1af668e1c27b179d936405ecf090783bd571
ccolorado1392 <c.colorado@uniandes.edu.co>
2022-09-19 20:02:26 -0500

    Update settings.py

--- reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -161,8 +161,8 @@ MQTT_HOST = "3.92.19.66"
 MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
-MQTT_USER = "admin"
-MQTT_PASSWORD = "admin"
+MQTT_USER = "user1"
+MQTT_PASSWORD = "123456"
 
 # Credenciales para el usuario publicador del bróker MQTT
 MQTT_USER_PUB = "user1"
