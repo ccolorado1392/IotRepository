b5548db21c631e3451ac88672da5f6d0427598a1
ccolorado1392 <c.colorado@uniandes.edu.co>
2022-09-19 20:45:20 -0500

    Update settings.py

--- reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ reto5/IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.92.19.66"
+MQTT_HOST = "3.234.205.158"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
